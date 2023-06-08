a9cdd510987441151689837b12c8227a7a2ec3c4
Melda Yanti <meldamelda729@gmail.com>
2023-06-08 08:31:33 +0800

    8 juni 2023

--- js/script.js
+++ js/script.js
@@ -35,26 +35,97 @@ document.addEventListener("click", function (e) {
   }
 });
 
-// Modal item detail
-const itemDetailModal = document.querySelector("#item-detail-modal");
-const itemDetailButtons = document.querySelectorAll(".item-detail-button");
+$(document).ready(() => {
+  // ambil data dari data.json dan tampilkan
+  $.getJSON("data/data.json", (dataJson) => {
+    const productItems = dataJson["products"];
 
-itemDetailButtons.forEach((btn) => {
-  btn.onclick = (e) => {
-    e.preventDefault();
-    itemDetailModal.style.display = "flex";
-  };
+    // perulangan per item
+    $.each(productItems, (i) => {
+      // ambil html
+      let textHtml = $("#products .product-card").html();
+      if (i > 0) {
+        $("#products .product-card")
+          .parent()
+          .append(`<div class="product-card" data-id="${i}">${textHtml}</div>`);
+      } else {
+        $("#products .product-card").attr("data-id", i);
+      }
+      let indexChild = i + 1;
+
+      // tampilkan gambar produk
+      $(
+        `#products .product-card:nth-child(${indexChild}) .product-image img`
+      ).attr("src", productItems[i]["picture"]);
+
+      // tampilkan nama produk
+      $(
+        `#products .product-card:nth-child(${indexChild}) .product-content h3`
+      ).html(productItems[i]["product_name"]);
+
+      // tampilkan harga produk
+      $(`#products .product-card:nth-child(${indexChild}) .product-price`).html(
+        productItems[i]["offer"] != "0"
+          ? `${rupiahFormat(productItems[i]["offer"])} <span> ${rupiahFormat(
+              productItems[i]["price"]
+            )}</span>`
+          : `${rupiahFormat(productItems[i]["price"])}`
+      );
+
+      // tampilkan bintang
+      const starCount = productItems[i]["stars"];
+
+      // remove class star full
+      $(
+        `#products .product-card:nth-child(${indexChild}) .product-stars svg`
+      ).removeClass("star-full");
+
+      // menambahkan kembali star full sesuai jumlah bintang
+      for (let j = 0; j < starCount; j++) {
+        $(
+          `#products .product-card:nth-child(${indexChild}) .product-stars svg:nth-child(${
+            j + 1
+          })`
+        ).addClass("star-full");
+      }
+    });
+    // Modal item detail
+    const itemDetailModal = document.querySelector("#item-detail-modal");
+    const itemDetailButtons = document.querySelectorAll(".item-detail-button");
+    console.log(itemDetailButtons.length);
+    const closeModal = document.querySelector("#modal-close-button");
+
+    // tampilkan modal
+    itemDetailButtons.forEach((btn) => {
+      btn.onclick = (e) => {
+        e.preventDefault();
+        $(itemDetailModal).attr("class", "modal show");
+
+        let productName = $(this)
+          .parent()
+          .parent()
+          .find(".product-content")
+          .html();
+        $(itemDetailModal).find(".product-content").html(productName);
+      };
+    });
+
+    // tombol close modal
+    $(closeModal).on("click", (e) => {
+      e.preventDefault();
+      $(itemDetailModal).attr("class", "modal");
+    });
+
+    // klik di luar modal
+    window.onclick = (e) => {
+      if (e.target === itemDetailModal) {
+        $(itemDetailModal).removeClass("show");
+      }
+    };
+  });
 });
 
-// tombol close modal
-document.querySelector(".modal .close-button").onclick = (e) => {
-  e.preventDefault();
-  itemDetailModal.style.display = "none";
-};
-
-// klik di luar modal
-window.onclick = (e) => {
-  if (e.target === itemDetailModal) {
-    itemDetailModal.style.display = "none";
-  }
+// format rupiah
+let rupiahFormat = (val) => {
+  return "Rp " + new Intl.NumberFormat("id-ID").format(val);
 };
